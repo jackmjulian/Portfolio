dead56082074b5d9bc1ef5602fd7e6d46719beb2
Jack Julian <117949276+jackmjulian@users.noreply.github.com>
2023-02-15 09:01:22 +0000

    Changes to Layout

---
+++ index.js
@@ -0,0 +1,29 @@
+var newcolor = document.getElementById('row2-right-content');
+var mouseOverFunction = function () {
+        this.style.color = '#85586F';
+};
+var mouseOutFunction = function (){
+        this.style.color = 'black';
+};
+newcolor.onmouseover = mouseOverFunction;
+newcolor.onmouseout = mouseOutFunction;
+
+var newcolor = document.getElementById('row3-left-content');
+var mouseOverFunction = function () {
+        this.style.color = '#C37777';
+};
+var mouseOutFunction = function (){
+        this.style.color = 'black';
+};
+newcolor.onmouseover = mouseOverFunction;
+newcolor.onmouseout = mouseOutFunction;
+
+/*var newcolor = document.getElementById('intro-text');
+var mouseOverFunction = function () {
+        this.style.color = '#D0B8A8';
+};
+var mouseOutFunction = function (){
+        this.style.color = 'black';
+};
+newcolor.onmouseover = mouseOverFunction;
+newcolor.onmouseout = mouseOutFunction; */
